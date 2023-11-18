a5bc27830fa517bd78f3f660c22196d19a57d139
Periclesbgf <pbgf@cesar.school>
2023-11-18 14:54:25 -0300

    copyrigth fix

--- main/include/udp.h
+++ main/include/udp.h
@@ -1,15 +1,15 @@
 /**
- * @file tcp.h
+ * @file udp.h
  * @author Péricles Buarque de Gusmão Filho (periclesbgf@gmail.com)
  * @brief 
  * @version 0.1
- * @date 2023-11-13
+ * @date 2023-11-18
  * 
  * @copyright Copyright (c) 2023
  * 
  */
-#ifndef _LED_TCP_H_
-#define _LED_TCP_H_
+#ifndef _LED_UDP_H_
+#define _LED_UDP_H_
 
 #include <string.h>
 #include <sys/param.h>
